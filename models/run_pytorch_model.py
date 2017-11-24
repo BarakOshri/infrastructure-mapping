39178bcd1797be989b44d7da87f14721789d06dd
Annie <anniehu4@gmail.com>
2017-11-24 14:50:20 -0800

    data augmentation

--- models/run_pytorch_model.py
+++ models/run_pytorch_model.py
@@ -201,6 +201,7 @@ class AddisDataset(Dataset):
 ####### Initialize Data
 
 data_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
